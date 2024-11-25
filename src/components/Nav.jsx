36b4eceac41884ea0ccb18de47ea32a01cd203f2
FulPlan Mutasim <74105091+fulplan@users.noreply.github.com>
2024-11-25 11:47:51 +0000

    snbsns

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
     return(
         <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
             <div className="container px-4 px-lg-5">
-                <Link className="navbar-brand" to="index.html">Start Bootstrap</Link>
+                <Link className="navbar-brand" to="/">Start Bootstrap</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
