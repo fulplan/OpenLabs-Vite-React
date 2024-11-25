4c23c3a4e31f6925a13eebb108ffa394d5bc8969
FulPlan Mutasim <74105091+fulplan@users.noreply.github.com>
2024-11-25 10:42:04 +0000

    Made Changes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return(
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" Component={Home}/>
+      <Route path="/about-us" Component={About}/>
+      <Route path="/contact-us" Component={Contact}/>
+     </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App;
