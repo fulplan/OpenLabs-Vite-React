8b4e5befea814eff521e6d7c45686204eb20ccf0
FulPlan Mutasim <74105091+fulplan@users.noreply.github.com>
2024-11-25 11:28:34 +0000

    snbjdbjd

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path="/" Component={Home}/>
-      <Route path="./Pages/About" Component={About}/>
-      <Route path="./Pages/Contact" Component={Contact}/>
+      <Route path="/about-us" Component={About}/>
+      <Route path="/contact-us" Component={Contact}/>
      </Routes>
     </BrowserRouter>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from  '/Pages/Home'
+import About from  '/Pages/About'
+import Contact from  '/Pages/Contact'
+
 // import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+   <Route path="/" element={Home}/>
+   <Route path="/about-us" element={About}/>
+   <Route path="/contact-us" element={Contact}/>
+  </Routes>
+ </BrowserRouter>
 )
